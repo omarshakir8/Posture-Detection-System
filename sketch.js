ab85e1c2373a82710a273d25b50d665fbb3df23e
omarshakir8 <omarshakir8@gmail.com>
2024-02-12 22:19:06 -0500

    Testing shapes

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
 function setup(){
-    createCanvas(400,400);
+    createCanvas(800,500);
 }
 
 function draw(){
-    background(color,[a])
+    background(200);
+    // 1. point
+    //point(200,200);
+    // 2. line
+    line(200,200,300,300)
+    //3. triangle
+    triangle(100,200,300,400,150,450)
+    //4.rectangle
+    rect(500,200,200,300)
+    //5 circle
+    ellipse(100)
 
 
 
