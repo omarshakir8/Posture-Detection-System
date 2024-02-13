9c6ff47b1aa109259a3bba9dd31109b6e97baf2a
omarshakir8 <omarshakir8@gmail.com>
2024-02-13 02:21:55 -0500

    Final Demo

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let capture;
-let posenet = null;
+let posenet;
 let noseX,noseY;
+let reyeX,reyeY;
+let singlePose,skeleton;
 
 
 
@@ -10,7 +12,7 @@ function setup(){
     capture.hide();
 
     posenet = ml5.poseNet(capture,modelLoaded);
-    posenet.on('pose',recievedPoses)
+    posenet.on('pose',recievedPoses);
     
 }
 
@@ -18,11 +20,10 @@ function recievedPoses(poses){
     console.log(poses);
 
     if(poses.length > 0){
-        singlePose = poses[0];
-        noseX = singlePose.pose.nose.x;
-        noseY = singlePose.pose.nose.y;
+        singlePose = poses[0].pose;
+        skeleton = poses[0].skeleton;
     }
-    console.log(noseX + " " + noseY);
+    
 
 }
 
@@ -33,7 +34,19 @@ function modelLoaded(){
 
 
 function draw(){
-    image(capture,0,0,800,600);
-    FileList(255,0,0);
-    ellipse(noseX,noseY, 30,30);
+    image(capture,0,0);
+    fill(255,0,0);
+
+
+    if(singlePose){
+        for(let i=0; i<singlePose.keypoints.length; i++){
+            ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y,20);
+        }
+        stroke(255,255,255);
+        strokeWeight(5);
+        for(let j=0; j<skeleton.length; j++){
+            line(skeleton[j][0].position.x, skeleton[j][0].position.y,skeleton[j][1].position.x,skeleton[j][1].position.y)
+        }
+    }
+    
 }
