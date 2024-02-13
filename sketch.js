ebb60bad5e86680e3248467ac89f43177649be67
omarshakir8 <omarshakir8@gmail.com>
2024-02-13 00:13:34 -0500

    Model Loaded

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
 let capture;
+let posenet = null;
 
 
 
 function setup(){
     createCanvas(800,500);
     capture = createCapture(VIDEO)
+    capture.hide();
+
+    posenet = ml5.poseNet(capture,modelLoaded);
+    
+}
+
+function modelLoaded(){
+    console.log('Model has loaded')
 }
 
 
