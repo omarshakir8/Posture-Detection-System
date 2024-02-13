c757a86b984684bc5a3987f432fe6b33430a9ea2
omarshakir8 <omarshakir8@gmail.com>
2024-02-13 02:02:43 -0500

    nose xy

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let capture;
 let posenet = null;
+let noseX,noseY;
 
 
 
@@ -9,16 +10,30 @@ function setup(){
     capture.hide();
 
     posenet = ml5.poseNet(capture,modelLoaded);
+    posenet.on('pose',recievedPoses)
     
 }
 
+function recievedPoses(poses){
+    console.log(poses);
+
+    if(poses.length > 0){
+        singlePose = poses[0];
+        noseX = singlePose.pose.nose.x;
+        noseY = singlePose.pose.nose.y;
+    }
+    console.log(noseX + " " + noseY);
+
+}
+
 function modelLoaded(){
-    console.log('Model has loaded')
+    console.log('Model has loaded');
 }
 
 
 
 function draw(){
     image(capture,0,0,800,600);
-    
+    FileList(255,0,0);
+    ellipse(noseX,noseY, 30,30);
 }
